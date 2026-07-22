1a18236bce26c86b7bac64fd7fd7ee5ca5633837
Jackie Tan <58161903+jackie9319@users.noreply.github.com>
2026-07-22 15:30:15 +0800

    Add files via upload

--- JS/data.js
+++ JS/data.js
@@ -45,5 +45,80 @@ window.POPUP_DATA = {
       zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/尊敬父母.mp3",
       en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/respect one's parents.mp3"
     }
-  }
+  },
+
+  "suka-hati": {
+    title: "suka hati",
+
+    image: "BM/SJKC/Tahun 2/Buku Teks/Images/suka hati.png",
+
+    bm: "berasa gembira atau seronok",
+    zh: "感到开心 / 高兴",
+    en: "feel happy or pleased",
+
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/berasa gembira atau seronok.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/感到开心,高兴.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/feel happy or pleased.mp3"
+    }
+  },
+
+    "ringan-tulang": {
+    title: "ringan tulang",
+
+    image: "BM/SJKC/Tahun 2/Buku Teks/Images/ringan tulang.png",
+
+    bm: "rajin bekerja atau rajin membantu",
+    zh: "勤劳 / 乐于助人",
+    en: "hardworking or always willing to help",
+
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/rajin bekerja atau rajin membantu.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/勤劳,乐于助人.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/hardworking or always willing to help.mp3"
+    }
+  },
+
+  "lipas-kudung": {
+    title: "lipas kudung",
+
+    image: "BM/SJKC/Tahun 2/Buku Teks/Images/lipas kudung.png",
+    bm: "bergerak atau bekerja dengan sangat cepat",
+    zh: "动作很快 / 做事非常迅速",
+    en: "move or work very quickly",
+
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/bergerak atau bekerja dengan sangat cepat.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/动作很快,做事非常迅速.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/move or work very quickly.mp3"
+    }
+  },
+
+  "jejak-langkah": {
+    title: "jejak langkah",
+
+    image: "BM/SJKC/Tahun 2/Buku Teks/Images/jejak langkah.png",
+
+    bm: "mengikut contoh atau perbuatan seseorang",
+    zh: "追随某人的脚步 / 效仿某人",
+    en: "follow in someone's footsteps or follow someone's example",
+
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/mengikut contoh atau perbuatan seseorang.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/追随某人的脚步,效仿某人.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/follow in someone's footsteps or follow someone's example.mp3"
+    }
+  },
+
+  "seni-bahasa": {
+    title: "Seni Bahasa",
+    bm: "Pak Cik Teoh membawa Guan Hong dan Ben Li ke kolam ikannya. Guan Hong dan Ben Li sangat suka hati. \nKolam ikan pak cik mereka sangat besar. Pekerja-pekerjanya ringan tulang. Mereka bekerja seperti lipas kudung. \nPak Cik Teoh: Guan Hong, bawa balik ikan ini. Kita minta nenek masak untuk makan tengah hari. \nGuan Hong: Baik, pak cik. \nGuan Hong dan Ben Li ingin mengikut jejak langkah pak cik mereka suatu hari nanti.",
+    zh: "张叔叔带 Guan Hong 和 Ben Li 到他的鱼塘。Guan Hong 和 Ben Li 感到非常开心。\n叔叔的鱼塘非常大。他的员工都很勤劳, 也很乐于帮忙。他们工作起来非常迅速。\n张叔叔说: “ Guan Hong, 把这条鱼带回去吧。我们请奶奶煮来当午餐。” \nGuan Hong 说：“好的, 叔叔。” \nGuan Hong 和 Ben Li 希望有一天能够追随叔叔的脚步，以叔叔为榜样。",
+    en: "Uncle Teoh took Guan Hong and Ben Li to his fish pond. Guan Hong and Ben Li felt very happy. \nTheir uncle's fish pond was very large. His workers were hardworking and always willing to help. They worked very quickly. \nUncle Teoh: Guan Hong, take this fish home. We will ask Grandma to cook it for lunch. \nGuan Hong: All right, \nUncle.Guan Hong and Ben Li hoped to follow in their uncle's footsteps one day.",
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/seni-bahasa-page15-petikan-BM.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/seni-bahasa-page15-petikan-CN.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/seni-bahasa-page15-petikan-EN.mp3"
+    }
+  },
 };
