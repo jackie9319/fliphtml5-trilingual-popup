e827a932f7c795e202fc3b7db444f77e443f3f56
Jackie Tan <58161903+jackie9319@users.noreply.github.com>
2026-07-22 10:36:54 +0800

    Add files via upload
    Upload trilingual popup

---
+++ JS/data.js
@@ -0,0 +1,49 @@
+window.POPUP_DATA = {
+  "bangun-awal": {
+    title: "bangun awal",
+    bm: "bangun awal",
+    zh: "早起",
+    en: "wake up early",
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/bangun awal.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/早起.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/wake up early.mp3"
+    }
+  },
+
+  "rajin-membaca": {
+    title: "rajin membaca",
+    bm: "rajin membaca",
+    zh: "勤于阅读 / 爱读书",
+    en: "read diligently / enjoy reading",
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/rajin membaca.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/勤于阅读 , 爱读书.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/read diligently , enjoy reading.mp3"
+    }
+  },
+
+  "bantu-keluarga": {
+    title: "bantu keluarga",
+    bm: "bantu keluarga",
+    zh: "帮助家人",
+    en: "help the family",
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/bantu keluarga.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/帮助家人.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/help the family.mp3"
+    }
+  },
+
+  "hormat-akan-ibu-bapa": {
+    title: "hormat akan ibu bapa",
+    bm: "hormat akan ibu bapa",
+    zh: "尊敬父母",
+    en: "respect one's parents",
+    audio: {
+      bm: "BM/SJKC/Tahun 2/Buku Teks/Audio/BM/hormat akan ibu bapa.mp3",
+      zh: "BM/SJKC/Tahun 2/Buku Teks/Audio/CN/尊敬父母.mp3",
+      en: "BM/SJKC/Tahun 2/Buku Teks/Audio/EN/respect one's parents.mp3"
+    }
+  }
+};
